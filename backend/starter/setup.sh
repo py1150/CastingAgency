8c588d74127b2ef6cf406d95a546fdd0bd088d37
py1150 <berndgaller@gmail.com>
2021-01-24 11:19:05 +0100

    setup.sh updated

--- backend/starter/setup.sh
+++ backend/starter/setup.sh
@@ -1,1 +1,8 @@
+export AUTH0_DOMAIN='https://py1150.eu.auth0.com/'
+export API_AUDIENCE='agency'
+export API_CLIENT_ID='EeYJIGaf3frKfj1nRwclF8nA9N88wYpz'
+export ALGORITHMS =['RS256']
 
+export CASTING_ASSISTANT="Bearer some_value"
+export EXECUTIVE_PRODUCER="Bearer some_value_02"
+export CASTING_DIRECTOR="Bearer some_value_03"
